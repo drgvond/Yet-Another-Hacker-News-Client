2442ed7c249e2faba3ffeef70d0b190a0a841508
Gabriel de Dietrich <gabriel.dedietrich@me.com>
2013-12-26 20:11:24 +0100

    Use segmented control to switch between news mode
    Removed the settings VC. Will bring back when needed. Also added compose button in storyboard (instead of add button in code). Finally, set HN’s orange as tint color in storyboard.

--- Hacker News/CHNEntriesTableViewController.m
+++ Hacker News/CHNEntriesTableViewController.m
@@ -10,11 +10,13 @@
 
 #import <HNKit/HNKit.h>
 #import "CHNWebViewController.h"
+#import "CHNEntryTableViewCell.h"
 
 @interface CHNEntriesTableViewController ()
 
 @property (strong, nonatomic) HNSession *currentSession;
 @property (strong, nonatomic) HNEntryList *submissions;
+@property (nonatomic) int newsMode;
 
 @end
 
@@ -33,22 +35,50 @@
 {
     [super viewDidLoad];
     
-    // TODO Add burger button instead
-//    self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
-    // TODO This one's for new submissions. Only visible if actual user, or ask to sign in.
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:nil];
-    self.navigationItem.rightBarButtonItem = addButton;
+//    self.tableView.rowHeight = 60;
+//    UINib *entryCellNib = [UINib nibWithNibName:@"CHNEntryTableViewCell" bundle:[NSBundle mainBundle]];
+//    [self.tableView registerNib:entryCellNib forCellReuseIdentifier:@"CHNEntryTableViewCell"];
+    [self.tableView registerClass:[CHNEntryTableViewCell class]
+           forCellReuseIdentifier:@"CHNEntryTableViewCell"];
     
     self.entryViewController = (CHNWebViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
     
     // Fetch news!
+    self.newsMode = 0;
     self.currentSession = [[HNAnonymousSession alloc] init];
     self.submissions = [HNEntryList session:self.currentSession entryListWithIdentifier:kHNEntryListIdentifierSubmissions];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(submissionsStartedLoading:) name:kHNObjectStartedLoadingNotification object:self.submissions];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(submissionsFinishedLoading:) name:kHNObjectFinishedLoadingNotification object:self.submissions];
     
+    [self beginLoadingEntries:self];
+}
+
+- (IBAction)beginLoadingEntries:(id)sender
+{
     [self.submissions beginLoading];
+}
+
+- (IBAction)newsModeChanged:(UISegmentedControl *)sender
+{
+    if (self.submissions.isLoading)
+        [self.submissions cancelLoading];
+    
+    self.newsMode = sender.selectedSegmentIndex;
+    switch (self.newsMode) {
+    case 0:
+        self.submissions.identifier =  kHNEntryListIdentifierSubmissions;
+        break;
+    case 1:
+        self.submissions.identifier =  kHNEntryListIdentifierNewSubmissions;
+        break;
+    case 2:
+        self.submissions.identifier =  kHNEntryListIdentifierAskSubmissions;
+        break;
+        
+    default:
+        break;
+    }
+    [self beginLoadingEntries:self];
 }
 
 - (void)submissionsStartedLoading:(NSNotification *)notification
@@ -60,6 +90,7 @@
 {
     NSLog(@"submissions loaded %@", notification.object);
     [self.tableView reloadData];
+    [self.refreshControl endRefreshing];
 }
 
 - (void)didReceiveMemoryWarning
@@ -82,56 +113,26 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CHNEntryTableViewCell"];
+    CHNEntryTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CHNEntryTableViewCell"];
 
     HNEntry *entry = self.submissions.entries[indexPath.row];
     cell.textLabel.text = entry.title;
-    NSString *detailText = [[NSString alloc] initWithFormat:@"%@ — %up — %uc — %@",
-                            entry.destination.host,
-                            entry.points, entry.children,
-                            entry.posted];
-    cell.detailTextLabel.text = detailText;
+//    NSString *detailText = [[NSString alloc] initWithFormat:@"%@ — %up — %uc — %@",
+//                            entry.destination.host,
+//                            entry.points, entry.children,
+//                            entry.posted];
+//    cell.detailTextLabel.text = detailText;
     return cell;
 }
 
-//- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-//{
-//    // Return NO if you do not want the specified item to be editable.
-//    return YES;
-//}
-//
-//- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-//{
-//    if (editingStyle == UITableViewCellEditingStyleDelete) {
-//        [_objects removeObjectAtIndex:indexPath.row];
-//        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-//    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-//        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-//    }
-//}
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath	 *)indexPath
 {
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
         HNEntry *entry = self.submissions.entries[indexPath.row];
         self.entryViewController.entry = entry;
     }
+    NSLog(@"row selected");
+    [self performSegueWithIdentifier:@"showArticle" sender:self];
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
