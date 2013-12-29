d1116f86e4c5ee04af423850e5a2ea8ace97edc7
Gabriel de Dietrich <gabriel.dedietrich@me.com>
2013-12-29 20:03:49 +0100

    Add dark orange color for entry cell detail text

--- Hacker News/UIColor+HNColors.m
+++ Hacker News/UIColor+HNColors.m
@@ -16,4 +16,10 @@
     return [UIColor colorWithRed:1.0 green:102.0/255.0 blue:0.0 alpha:1.0];
 }
 
++ (UIColor *)hnDarkOrangeColor
+{
+    // Header color from https://news.ycombinator.com is #ff6600
+    return [UIColor colorWithRed:190.0/255.0 green:105.0/255.0 blue:55.0/255.0 alpha:1.0];
+}
+
 @end
